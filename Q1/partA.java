dbe88a5a5fdf1cef00828bf2f2e1ce0515467a6d
hansTeachesCS <152180694+hansTeachesCS@users.noreply.github.com>
2025-05-12 11:30:06 -0400

    Update partA.java

--- Q1/partA.java
+++ Q1/partA.java
@@ -4,12 +4,12 @@ public int walkDogs(int hour) {
     // find out from DogWalkCompany how many dogs at hour
     int numDogs = company.numAvailableDogs(hour);
 
-    // if walker can walk more dogs than available
-    if (maxDogs > numDogs) {
+    // if walker can walk more (or equal) dogs than available
+    if (maxDogs >= numDogs) {
         company.updateDogs(hour, numDogs);
         return numDogs;
     } 
-    // if there are more (or equal) dogs than the walker can handle
+    // if there are more dogs than the walker can handle
     company.updateDogs(hour, maxDogs);
     return maxDogs;
 }
