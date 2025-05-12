ba95f97cc7319a6cb113c5926b28d697db1b974a
hansTeachesCS <152180694+hansTeachesCS@users.noreply.github.com>
2025-05-12 11:45:55 -0400

    initialize variable and loop

--- Q1/partB.java
+++ Q1/partB.java
@@ -1,3 +1,9 @@
 public int dogWalkShift(int startHour, int endHour) {
+    int moneyEarned = 0;
 
+    for (int hour = startHour; hour <= endHour; hour++) {
+        
+    }
+
+    return moneyEarned;
 }
